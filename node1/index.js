1a2d9c39921a8473a83635c4f0c4a37549a83745
elena25466551 <elenacastrodm76@gmail.com>
2023-09-20 10:25:38 -0300

    Entrega Balanceador

---
+++ node1/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import mysql from 'mysql2';
+
+const port = 80;
+const app = express();
+
+const pool = mysql.createPool({
+  host: '172.17.0.2',
+  user: 'root',
+  password: '1234',
+  database: 'prueba'
+}).promise()
+
+
+//RUTAS
+app.get("/", async (req, res) => {
+  const result = await pool.query('SELECT * FROM ALUMNOS');
+  res.json(['contenedor: 1', result[0]]);
+});
+
+
+app.listen(port, () => {
+  console.log(`Servidor en ejecución en puerto:${port}`)
+});
